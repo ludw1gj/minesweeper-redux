57d97c783cec6af854ca336d748679f519b1a6d7
Robert <ludw1gj@icloud.com>
2022-12-22 16:08:04 +1100

    Fix typo

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
     lib: {
       entry: resolve(__dirname, 'src/index.ts'),
       name: 'minesweeper-redux',
-      fileName: 'minesweep-redux',
+      fileName: 'minesweeper-redux',
     },
   },
   plugins: [dts()],
